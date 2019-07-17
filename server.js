899c717cb0631aaf13cd0a9376d24d4913eee876
takeohiraki <takeohiraki@gmail.com>
2019-07-16 19:57:38 -0700

    added procfile

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 
 // Routes
